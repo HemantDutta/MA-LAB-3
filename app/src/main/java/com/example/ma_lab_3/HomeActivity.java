d9c7cfe857836699503524e9510334f10a544fbf
HemantDutta <hemantnorddutta2@gmail.com>
2023-07-06 08:58:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ma_lab_3/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.ma_lab_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
