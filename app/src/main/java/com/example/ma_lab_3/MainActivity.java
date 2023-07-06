336ce3e8faf4052ce6a611ac1045566d1c1f0b98
HemantDutta <hemantnorddutta2@gmail.com>
2023-07-06 19:46:44 +0530

    Create Login Function

--- app/src/main/java/com/example/ma_lab_3/MainActivity.java
+++ app/src/main/java/com/example/ma_lab_3/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.ma_lab_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+
+        if(username.getText().toString() == "Hemant" && password.getText().toString() == "admin"){
+            String message = "Welcome "+username.getText().toString();
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+            intent.putExtra("username", username.getText().toString());
+            intent.putExtra("password", password.getText().toString());
+
+            startActivity(intent);
+        }
     }
 }
